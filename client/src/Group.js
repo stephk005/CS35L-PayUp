cff77d07794d84e3f47169d89c631a1e2e0ca3ab
stephk005 <stephaniekwu8@gmail.com>
2023-05-16 23:51:59 -0700

    Updated UI display for groups to display transactions Created a temporary database for clearer display of each group. Stored group in a dictionary array with placeholder variables like group name, lender, and borrower that map to dollar amounts. This will allow for easier integration with the backend we intend to implement soon.

--- client/src/Group.js
+++ client/src/Group.js
@@ -3,16 +3,72 @@ import "./Group.css";
 import Header from "./Header";
 
 export default function Group() {
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
   const groupname = "San Diego Trip";
-  const yourTransactions = [-10, -5, 8];
+  const temp_database = [
+    {
+      "Dinner": {
+        lender: { "Ian": 50 },
+        borrower: {
+          "Steph": 20,
+          "Cris": 10,
+          "Henry": 20
+        }
+      }
+    },
+    {
+      "Uber": {
+        lender: { "Henry": 30 },
+        borrower: {
+          "Steph": 10,
+          "Cris": 10
+        }
+      }
+    }
+  ];
+
+  const renderTransactions = (
+    <div>
+      {temp_database.map((entry) => {
+        const key = Object.keys(entry)[0]; // Get the key of the entry
+        const transaction = entry[key]; // Get the transaction object
+        const lender = Object.keys(transaction.lender)[0];
+
+        // Format and render the lender information
+        const lenderInfo = Object.entries(transaction.lender)
+          .map(([name, amount]) => `${name} paid $${amount}.`)
+          .join(' ');
+
+        // Format and render the borrower information as an array
+        const borrowerInfo = Object.entries(transaction.borrower).map(
+          ([name, amount]) => `${name} owes ${lender} $${amount}.`
+        );
+
+        // Render the transaction information
+        return (
+          <div className="transaction" key={key}>
+            <div className="title">{key}</div>
+            <div className="lender">{lenderInfo}</div>
+            <div className="borrower">
+              {borrowerInfo.map((info, index) => (
+                <div key={index}>{info}</div>
+              ))}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 
   return (
-    <div>
-      <Header/>
-      <center>
-        <div className="title">{groupname}</div>
-        <label>Transactions </label>
-      </center>
+  <div>
+    <Header/>
+    <div className="group">
+      <div className="title">{groupname}</div>
+      {renderTransactions};
     </div>
+  </div>
   );
 }
