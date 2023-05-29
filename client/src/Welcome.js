0a3c983c30b9c7f053bf18602e1bcd0054691d90
stephk005 <stephaniekwu8@gmail.com>
2023-05-29 01:35:01 -0700

    Additional UI updates to the welcome page. Some changes to the welcome page were not staged properly.

--- client/src/Welcome.js
+++ client/src/Welcome.js
@@ -2,17 +2,20 @@ import React, { useState } from "react";
 import "./Welcome.css";
 import {Link} from "react-router-dom"
 import Header from "./Header";
+import logoImage from "./logo.png";
 
 export default function Welcome() {
     return (
       <div>
         <Header/>
         <div className="welcome">
-          <div className="intro">
-                <p>Paragraph about payup and other introduction stuff. </p>
-          </div>
+          <div className="text-container">
+            <h1 className="intro">PayUp</h1>
+            <h2 className="slogan">Expenses made easy.</h2>
+            <h2 className="slogan">Sign up today.</h2>
+            </div>
+        <img src={logoImage} className="logo" />
         </div>
       </div>
-
-      );
+    );
 }
