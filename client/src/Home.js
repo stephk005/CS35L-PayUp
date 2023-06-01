eaf81fa30bc2585b268c94a428c6335575c63d9a
Ian Shih <ianshih22856@gmail.com>
2023-05-31 21:20:02 -0700

    implemented adding friends to db has some errors

--- client/src/Home.js
+++ client/src/Home.js
@@ -6,6 +6,65 @@ import { Link } from "react-router-dom";
 
 
 export default function Home(){
+    const handleSubmit = async (event) => {
+    
+        //Prevent page reload
+        event.preventDefault();
+
+        var {friendname} = document.forms[0];
+        // console.log("friendname: ", friendname.value);
+
+
+        const usernameResp = await fetch(`http://localhost:5050/record/user/username/${friendname.value}`);
+
+        if (usernameResp.statusText !== "Not Found" && !usernameResp.ok){ //if friend isnt found
+            const message = `An error has occured: ${usernameResp.statusText}`;
+            window.alert(message);
+            return;
+        }
+        //if friend username is found 
+        let friendData;
+        try{
+            friendData = await usernameResp.json();
+            // console.log(friendData);
+
+        } catch (e) {
+        console.error(e);
+        }
+
+        if (friendData)
+        {
+            let friendID = {id: friendData._id};
+            // console.log("friend id: ", friendID.id);            
+            let currentUser = JSON.parse(localStorage.getItem('currentuser'));
+            // (`http://localhost:5050/record/user/${user}`)
+            // console.log("current user", currentUser._id);
+            if (friendID._id == currentUser._id)
+            {
+                window.alert("Cannot add yourself. ");
+                return;
+            }
+            const url = `http://localhost:5050/record/user/insert/friend/${currentUser._id}`;
+            let result = await fetch(url, {
+                method: "PATCH",
+                headers: {
+                    'Content-Type': 'application/json'   // This needs to be included for proper parsing
+                },
+                body: JSON.stringify(friendID)
+            });
+            // console.log(result.status);
+            if(result.status !== 201) console.log("Error inserting friend ID");
+            else {  // Print updated user document
+                console.log("Successfully inserted friend!");
+                let userURL = 'http://localhost:5050/record/user/${currentUser._id}'
+                let res = await fetch(userURL);
+                if(res.status === 200){
+                    let user = await res.json();
+                    console.log("updated user: ", user);
+                } else console.log("Error fetching user");
+            }
+        }
+    }
     return (
         <div className="HomeScreen">
             <HomeHeader/>
@@ -25,8 +84,14 @@ export default function Home(){
                 <div className="FriendsGroupsScreen">
                     <label>Friends </label>
                     <div className="FriendsListContent">
+                            <form onSubmit={handleSubmit}>  
+                                <div className="searchfriend">
+                                    <input type="text" name = "friendname" placeholder="Search for friend" />
+                                    <input type="submit" value="Add"/>
+                                </div>
+                            </form>
                             <ul className="friendsList">{loadFriendsList()}</ul> 
-                    </div>
+                        </div>
                     <label>Groups</label>
                     <div className="FriendsListContent">
                             <ul className="friendsList">{loadGroupsList()}</ul> 
